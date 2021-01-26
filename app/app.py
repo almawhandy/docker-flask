6aa44551c981647b03a358dd99e03d90df7cc386
Imerann83 <dany.gravina@alice.it>
2021-01-26 11:16:52 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Bentornato! Sono contento di vederti!"
+    return "Bentornato! Non sono contento di vederti!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
